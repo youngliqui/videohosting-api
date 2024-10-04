b2db1011a066b3e4de65301734cfa3c5e12e7d29
youngliqui <super.pav-79@yandex.by>
2024-10-04 23:57:08 +0300

    init

---
+++ src/test/java/ru/clevertec/videohosting_api/VideohostingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.clevertec.videohosting_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideohostingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
